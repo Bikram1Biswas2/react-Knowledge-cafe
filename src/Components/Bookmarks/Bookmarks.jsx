8519dcd32376ddbb8c0d0da91c0e358dd30591c8
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2023-10-31 11:05:48 +0530

    second commit

---
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bookmarks = () => {
+    return (
+        <div className='md:h-1/3'>
+            <h4>Bookmarks</h4>
+        </div>
+    );
+};
+
+export default Bookmarks;
